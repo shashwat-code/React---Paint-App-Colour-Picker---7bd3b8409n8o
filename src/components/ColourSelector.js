453e0be7bb1add2834781defbbabcd26525ac984
shashwat-code <91280518+shashwat-code@users.noreply.github.com>
2022-03-02 20:05:42 +0530

    change selection

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -3,9 +3,10 @@ import React from 'react';
 const ColourSelector = (props) => {
   const { config, selectNextBackground } = props
   const { background } = config;
+  console.log("entering this phase",config)
   return (
-    <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
-      {/* label should come here */}
+    <button className={config.classname} onClick={() => selectNextBackground({background: background})}>
+      {config.label}
     </button>
   )
 }
